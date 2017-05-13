8884a7d9d62296b41a96db89063665d39baeb6c3
bvillasen <bvillasen@gmail.com>
2017-05-12 17:37:40 -0700

    added CIC interpolation for density and force

--- pMesh.jl
+++ pMesh.jl
@@ -8,8 +8,8 @@ using tools
 
 
 const nParticles = 128^3
-const totalSteps = 250
-const dt = 0.002
+const totalSteps = 3000
+const dt = 2e-10
 
 
 const Gconst    = 1 #m**2/(kg*s**2)
@@ -43,12 +43,13 @@ c_pos_x = linspace( x_min + dx/2, x_max - dx/2, nCells_x)
 c_pos_y = linspace( y_min + dy/2, y_max - dy/2, nCells_y)
 c_pos_z = linspace( z_min + dz/2, z_max - dz/2, nCells_z)
 
-#Particles Positions
-#Random Uniform
+##########################################################################
+# Particles Positions
+# # Random Uniform
 # p_pos_x = (x_max - x_min)*rand( nParticles ) + x_min
 # p_pos_y = (y_max - y_min)*rand( nParticles ) + y_min
 # p_pos_z = (z_max - z_min)*rand( nParticles ) + z_min
-
+#########################################################################
 #Spherically uniform random distribution for initial positions
 initialTheta = 2*pi*rand(nParticles)
 initialPhi = acos( 2*rand(nParticles) - 1)
@@ -66,6 +67,22 @@ p_pos_z = initialR .* cos(initialPhi) + center_z
 p_vel_x = zeros( nParticles )
 p_vel_y = zeros( nParticles )
 p_vel_z = zeros( nParticles )
+##########################################################################
+# # Orbiting Particles initial conditions
+# D = dx*nCells_x/8
+# r = D/2
+# v_circ = sqrt( Gconst * r / D^2 )
+# p_pos_x = [ 0.5-r 0.5+r ]
+# p_pos_y = [ 0.5 0.5 ]
+# p_pos_z = [ 0.5-dz/2 0.5-dz/2 ]
+# p_vel_x = [ 0. 0. ]
+# p_vel_y = [ v_circ -v_circ ]
+# p_vel_z = [ 0. 0. ]
+
+
+
+
+
 
 dx_all = zeros( nParticles )
 dy_all = zeros( nParticles )
@@ -74,9 +91,14 @@ dz_all = zeros( nParticles )
 
 # #Cloud-in-Cell density interpolation
 # #First get  Nearest Grid Point NGP
-# idxs_x = floor( Int, p_pos_x/dx ) + 1
-# idxs_y = floor( Int, p_pos_y/dy ) + 1
-# idxs_z = floor( Int, p_pos_z/dz ) + 1
+# function get_
+# idxs_x = floor( Int, (p_pos_x - dx/2)/dx ) + 1
+# idxs_y = floor( Int, (p_pos_y - dy/2)/dy ) + 1
+# idxs_z = floor( Int, (p_pos_z - dz/2)/dz ) + 1
+# inside_x = idxs_x .> x_min
+# inside_y = idxs_y .> y_min
+# inside_z = idxs_z .> z_min
+# p_inside = inside_z .* inside_y .* inside_x
 # for i in 1:nParticles
 #   dx_all[i] = p_pos_x[i] - c_pos_x[idxs_x[i]]
 #   dy_all[i] = p_pos_y[i] - c_pos_y[idxs_y[i]]
@@ -91,7 +113,27 @@ dz_all = zeros( nParticles )
 
 #Array for particles inside the box
 p_inside = ones( Bool, nParticles )
-function get_particles_outside( p_inside, p_pos_x, p_pos_y, p_pos_z,
+function get_particles_outside_CIC( p_inside, p_pos_x, p_pos_y, p_pos_z,
+             x_min, x_max, y_min, y_max, z_min, z_max, dx, dy, dz )
+  for i in 1:nParticles
+    x, y, z = p_pos_x[i], p_pos_y[i], p_pos_z[i]
+    if  ( x<(x_min+2*dx) || x>(x_max-2*dx) )
+      p_inside[i] = false
+      continue
+    end
+    if  ( y<(y_min+2*dy) || y>(y_max-2*dy) )
+      p_inside[i] = false
+      continue
+    end
+    if  ( z<(z_min+2*dz) || z>(z_max-2*dz) )
+      p_inside[i] = false
+      continue
+    end
+  end
+end
+
+
+function get_particles_outside_NPG( p_inside, p_pos_x, p_pos_y, p_pos_z,
              x_min, x_max, y_min, y_max, z_min, z_max)
   for i in 1:nParticles
     x, y, z = p_pos_x[i], p_pos_y[i], p_pos_z[i]
@@ -109,11 +151,10 @@ function get_particles_outside( p_inside, p_pos_x, p_pos_y, p_pos_z,
     end
   end
 end
-get_particles_outside( p_inside, p_pos_x, p_pos_y, p_pos_z, x_min, x_max, y_min, y_max, z_min, z_max)
 
 outputDir = ""
 fileName =  outputDir * "p_pos.h5"
-outFile = h5open( fileName, "w")
+# outFile = h5open( fileName, "w")
 
 function writeSnapshot( n, name, data, outFile; stride=1)
   snapNumber = n < 100 ? "0$(n)" : "$(n)"
@@ -124,15 +165,55 @@ function writeSnapshot( n, name, data, outFile; stride=1)
   outFile[ key * "_pos_z" ] = map( Float32, pos_z[1:stride:end] )
 end
 
-writeSnapshot( 0, "", [p_pos_x, p_pos_y, p_pos_z], outFile, stride=8 )
+# writeSnapshot( 0, "", [p_pos_x, p_pos_y, p_pos_z], outFile, stride=8 )
 
-function get_density_NGP( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_y, nCells_z, dx, dy, dz )
+function get_indxs_CIC( dx, dy, dz, p_pos_x, p_pos_y, p_pos_z )
+  idxs_x = floor( Int, (p_pos_x - 0.5*dx)/dx ) + 1
+  idxs_y = floor( Int, (p_pos_y - 0.5*dy)/dy ) + 1
+  idxs_z = floor( Int, (p_pos_z - 0.5*dz)/dz ) + 1
+  return [ idxs_x, idxs_y, idxs_z ]
+end
+
+function get_density_CIC( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_y, nCells_z, dx, dy, dz, c_pos_x, c_pos_y, c_pos_z )
+  idxs_x, idxs_y, idxs_z = get_indxs_CIC( dx, dy, dz, p_pos_x, p_pos_y, p_pos_z )
   #Density array
   rho = zeros( nCells_z, nCells_y, nCells_x )
-  #Get Density Nearest Grid Point NGP
+  for i in 1:nParticles
+    if !p_inside[i]
+      continue
+    end
+    idx_x, idx_y, idx_z = idxs_x[i], idxs_y[i], idxs_z[i]
+    delta_x = 1 - (p_pos_x[i] - c_pos_x[idx_x])/dx
+    delta_y = 1 - (p_pos_y[i] - c_pos_y[idx_y])/dy
+    delta_z = 1 - (p_pos_z[i] - c_pos_z[idx_z])/dz
+    # println("$(delta_x)    $(delta_y)    $(delta_z)    ")
+    rho[idx_z, idx_y, idx_x] += delta_x * delta_y * delta_z
+    rho[idx_z, idx_y, idx_x+1] += (1-delta_x) * delta_y * delta_z
+    rho[idx_z, idx_y+1, idx_x] += delta_x * (1-delta_y) * delta_z
+    rho[idx_z+1, idx_y, idx_x] += delta_x * delta_y * (1-delta_z)
+    rho[idx_z+1, idx_y+1, idx_x] += delta_x * (1-delta_y) * (1-delta_z)
+    rho[idx_z, idx_y+1, idx_x+1] += (1-delta_x) * (1-delta_y) * delta_z
+    rho[idx_z+1, idx_y, idx_x+1] += (1-delta_x) * delta_y * (1-delta_z)
+    rho[idx_z+1, idx_y+1, idx_x+1] += (1-delta_x) * (1-delta_y) * (1-delta_z)
+  end
+  rho /= dx*dy*dz
+  return rho, idxs_x, idxs_y, idxs_z
+end
+
+
+
+
+function get_indxs_NPG( dx, dy, dz, p_pos_x, p_pos_y, p_pos_z )
   idxs_x = floor( Int, p_pos_x/dx ) + 1
   idxs_y = floor( Int, p_pos_y/dy ) + 1
   idxs_z = floor( Int, p_pos_z/dz ) + 1
+  return [ idxs_x, idxs_y, idxs_z ]
+end
+
+function get_density_NGP( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_y, nCells_z, dx, dy, dz )
+  idxs_x, idxs_y, idxs_z = get_indxs_NPG( dx, dy, dz, p_pos_x, p_pos_y, p_pos_z )
+  #Density array
+  rho = zeros( nCells_z, nCells_y, nCells_x )
   for i in 1:nParticles
     if !p_inside[i]
       continue
@@ -140,7 +221,7 @@ function get_density_NGP( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_
     idx_x, idx_y, idx_z = idxs_x[i], idxs_y[i], idxs_z[i]
     rho[idx_z, idx_y, idx_x] += 1
   end
-  rho *= dx*dy*dz
+  rho /= dx*dy*dz
   return rho, idxs_x, idxs_y, idxs_z
 end
 
@@ -155,7 +236,139 @@ function get_potential( density, G, fft_plan_f, fft_plan_b)
 end
 
 
+function get_grav_force_CIC( phi, dx, dy, dz, i, idx_x, idx_y, idx_z, p_pos_x, p_pos_y, p_pos_z, c_pos_x, c_pos_y, c_pos_z )
+  delta_x = 1 - (p_pos_x[i] - c_pos_x[idx_x])/dx
+  delta_y = 1 - (p_pos_y[i] - c_pos_y[idx_y])/dy
+  delta_z = 1 - (p_pos_z[i] - c_pos_z[idx_z])/dz
+  #FOR bottom-left cell
+  # X Force component
+  phi_l = phi[idx_z, idx_y, idx_x-1]
+  phi_r = phi[idx_z, idx_y, idx_x+1]
+  g_x_bl = -( phi_r - phi_l ) / ( 2*dx )
+  # Y Force component
+  phi_d = phi[idx_z, idx_y-1, idx_x]
+  phi_u = phi[idx_z, idx_y+1, idx_x]
+  g_y_bl = -( phi_u - phi_d ) / ( 2*dy )
+  # Z Force component
+  phi_b = phi[idx_z-1, idx_y, idx_x]
+  phi_t = phi[idx_z+1, idx_y, idx_x]
+  g_z_bl = -( phi_t - phi_b ) / ( 2*dz )
 
+  #FOR bottom-right
+  # X Force component
+  phi_l = phi[idx_z, idx_y, idx_x]
+  phi_r = phi[idx_z, idx_y, idx_x+2]
+  g_x_br = -( phi_r - phi_l ) / ( 2*dx )
+  # Y Force component
+  phi_d = phi[idx_z, idx_y-1, idx_x+1]
+  phi_u = phi[idx_z, idx_y+1, idx_x+1]
+  g_y_br = -( phi_u - phi_d ) / ( 2*dy )
+  # Z Force component
+  phi_b = phi[idx_z-1, idx_y, idx_x+1]
+  phi_t = phi[idx_z+1, idx_y, idx_x+1]
+  g_z_br = -( phi_t - phi_b ) / ( 2*dz )
+
+  #FOR bottom-up
+  # X Force component
+  phi_l = phi[idx_z, idx_y+1, idx_x-1]
+  phi_r = phi[idx_z, idx_y+1, idx_x+1]
+  g_x_bu = -( phi_r - phi_l ) / ( 2*dx )
+  # Y Force component
+  phi_d = phi[idx_z, idx_y, idx_x]
+  phi_u = phi[idx_z, idx_y+2, idx_x]
+  g_y_bu = -( phi_u - phi_d ) / ( 2*dy )
+  # Z Force component
+  phi_b = phi[idx_z-1, idx_y+1, idx_x]
+  phi_t = phi[idx_z+1, idx_y+1, idx_x]
+  g_z_bu = -( phi_t - phi_b ) / ( 2*dz )
+
+  #FOR bottom-right-up
+  # X Force component
+  phi_l = phi[idx_z, idx_y+1, idx_x]
+  phi_r = phi[idx_z, idx_y+1, idx_x+2]
+  g_x_bru = -( phi_r - phi_l ) / ( 2*dx )
+  # Y Force component
+  phi_d = phi[idx_z, idx_y, idx_x+1]
+  phi_u = phi[idx_z, idx_y+2, idx_x+1]
+  g_y_bru = -( phi_u - phi_d ) / ( 2*dy )
+  # Z Force component
+  phi_b = phi[idx_z-1, idx_y+1, idx_x+1]
+  phi_t = phi[idx_z+1, idx_y+1, idx_x+1]
+  g_z_bru = -( phi_t - phi_b ) / ( 2*dz )
+
+  #FOR top-left
+  # X Force component
+  phi_l = phi[idx_z+1, idx_y, idx_x-1]
+  phi_r = phi[idx_z+1, idx_y, idx_x+1]
+  g_x_tl = -( phi_r - phi_l ) / ( 2*dx )
+  # Y Force component
+  phi_d = phi[idx_z+1, idx_y-1, idx_x]
+  phi_u = phi[idx_z+1, idx_y+1, idx_x]
+  g_y_tl = -( phi_u - phi_d ) / ( 2*dy )
+  # Z Force component
+  phi_b = phi[idx_z, idx_y, idx_x]
+  phi_t = phi[idx_z+2, idx_y, idx_x]
+  g_z_tl = -( phi_t - phi_b ) / ( 2*dz )
+
+  #FOR top-right
+  # X Force component
+  phi_l = phi[idx_z+1, idx_y, idx_x]
+  phi_r = phi[idx_z+1, idx_y, idx_x+2]
+  g_x_tr = -( phi_r - phi_l ) / ( 2*dx )
+  # Y Force component
+  phi_d = phi[idx_z+1, idx_y-1, idx_x+1]
+  phi_u = phi[idx_z+1, idx_y+1, idx_x+1]
+  g_y_tr = -( phi_u - phi_d ) / ( 2*dy )
+  # Z Force component
+  phi_b = phi[idx_z, idx_y, idx_x+1]
+  phi_t = phi[idx_z+2, idx_y, idx_x+1]
+  g_z_tr = -( phi_t - phi_b ) / ( 2*dz )
+
+  #FOR top-up
+  # X Force component
+  phi_l = phi[idx_z+1, idx_y+1, idx_x-1]
+  phi_r = phi[idx_z+1, idx_y+1, idx_x+1]
+  g_x_tu = -( phi_r - phi_l ) / ( 2*dx )
+  # Y Force component
+  phi_d = phi[idx_z+1, idx_y, idx_x]
+  phi_u = phi[idx_z+1, idx_y+2, idx_x]
+  g_y_tu = -( phi_u - phi_d ) / ( 2*dy )
+  # Z Force component
+  phi_b = phi[idx_z, idx_y+1, idx_x]
+  phi_t = phi[idx_z+2, idx_y+1, idx_x]
+  g_z_tu = -( phi_t - phi_b ) / ( 2*dz )
+
+  #FOR top-right-up
+  # X Force component
+  phi_l = phi[idx_z+1, idx_y+1, idx_x]
+  phi_r = phi[idx_z+1, idx_y+1, idx_x+2]
+  g_x_tru = -( phi_r - phi_l ) / ( 2*dx )
+  # Y Force component
+  phi_d = phi[idx_z+1, idx_y, idx_x+1]
+  phi_u = phi[idx_z+1, idx_y+2, idx_x+1]
+  g_y_tru = -( phi_u - phi_d ) / ( 2*dy )
+  # Z Force component
+  phi_b = phi[idx_z, idx_y+1, idx_x+1]
+  phi_t = phi[idx_z+2, idx_y+1, idx_x+1]
+  g_z_tru = -( phi_t - phi_b ) / ( 2*dz )
+
+  g_x = g_x_bl*delta_x*delta_y*delta_z         + g_x_br*(1-delta_x)*delta_y*delta_z +
+        g_x_bu*delta_x*(1-delta_y)*delta_z     + g_x_bru*(1-delta_x)*(1-delta_y)*delta_z +
+        g_x_tl*delta_x*delta_y*(1-delta_z)     + g_x_tr*(1-delta_x)*delta_y*(1-delta_z) +
+        g_x_tu*delta_x*(1-delta_y)*(1-delta_z) + g_x_tru*(1-delta_x)*(1-delta_y)*(1-delta_z)
+
+  g_y = g_y_bl*delta_x*delta_y*delta_z         + g_y_br*(1-delta_x)*delta_y*delta_z +
+        g_y_bu*delta_x*(1-delta_y)*delta_z     + g_y_bru*(1-delta_x)*(1-delta_y)*delta_z +
+        g_y_tl*delta_x*delta_y*(1-delta_z)     + g_y_tr*(1-delta_x)*delta_y*(1-delta_z) +
+        g_y_tu*delta_x*(1-delta_y)*(1-delta_z) + g_y_tru*(1-delta_x)*(1-delta_y)*(1-delta_z)
+
+  g_z = g_z_bl*delta_x*delta_y*delta_z         + g_z_br*(1-delta_x)*delta_y*delta_z +
+        g_z_bu*delta_x*(1-delta_y)*delta_z     + g_z_bru*(1-delta_x)*(1-delta_y)*delta_z +
+        g_z_tl*delta_x*delta_y*(1-delta_z)     + g_z_tr*(1-delta_x)*delta_y*(1-delta_z) +
+        g_z_tu*delta_x*(1-delta_y)*(1-delta_z) + g_z_tru*(1-delta_x)*(1-delta_y)*(1-delta_z)
+
+  return [ g_x g_y g_z ]
+end
 
 function get_grav_force_NGP( phi, idx_x, idx_y, idx_z )
   # X Force component
@@ -208,6 +421,23 @@ function initialize_FFT( nCells_x, nCells_y, nCells_z, Lx, Ly, Lz, dx, dy, dz )
   return [ G, fft_plan_fwd, fft_plan_bkwd ]
 end
 
+function update_particles_CIC( dt, dx, dy, dz, phi, idxs_x, idxs_y, idxs_z, p_inside,
+                               p_pos_x, p_pos_y, p_pos_z,
+                               p_vel_x, p_vel_y, p_vel_z,
+                               c_pos_x, c_pos_y, c_pos_z  )
+  for i in 1:nParticles
+    if !p_inside[i]
+      continue
+    end
+    idx_x, idx_y, idx_z = idxs_x[i], idxs_y[i], idxs_z[i]
+    g_x, g_y, g_z = get_grav_force_CIC( phi, dx, dy, dz, i, idx_x, idx_y, idx_z, p_pos_x, p_pos_y, p_pos_z, c_pos_x, c_pos_y, c_pos_z )
+    update_leapfrog( i, dt, p_pos_x, p_pos_y, p_pos_z, p_vel_x, p_vel_y, p_vel_z, g_x, g_y, g_z )
+  end
+end
+
+
+
+
 function update_particles_NGP( dt, phi, idxs_x, idxs_y, idxs_z, p_inside,
                                p_pos_x, p_pos_y, p_pos_z,
                                p_vel_x, p_vel_y, p_vel_z,  )
@@ -222,47 +452,84 @@ function update_particles_NGP( dt, phi, idxs_x, idxs_y, idxs_z, p_inside,
 end
 
 
-G, fft_plan_fwd, fft_plan_bkwd = initialize_FFT( nCells_x, nCells_y, nCells_z, Lx, Ly, Lz, dx, dy, dz )
 
-function advance_step_NGP( nStep, nCells_x, nCells_y, nCells_z, dx, dy, dz, p_inside,
+function plot_slides( nStep, cut, rho, phi )
+  img = log( 1e-7*rho[cut,:,:] + 1 )
+  clf()
+  imshow(img)
+  colorbar()
+  title( "Density" )
+  savefig("images/density_$(nStep/2 -1 ).png")
+  img = phi[cut,:,:]
+  clf()
+  imshow(img)
+  colorbar()
+  title( "Potential" )
+  savefig("images/potential_$(nStep/2 -1 ).png")
+end
+
+function advance_step_CIC( nStep, nCells_x, nCells_y, nCells_z, dx, dy, dz, p_inside,
                            p_pos_x, p_pos_y, p_pos_z, p_vel_x, p_vel_y, p_vel_z,
+                           c_pos_x, c_pos_y, c_pos_z,
                            G, fft_plan_fwd, fft_plan_bkwd )
-  get_particles_outside( p_inside, p_pos_x, p_pos_y, p_pos_z, x_min, x_max, y_min, y_max, z_min, z_max)
-  rho, idxs_x, idxs_y, idxs_z = get_density_NGP( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_y, nCells_z, dx, dy, dz )
+  get_particles_outside_CIC( p_inside, p_pos_x, p_pos_y, p_pos_z, x_min, x_max, y_min, y_max, z_min, z_max, dx, dy, dz)
+  rho, idxs_x, idxs_y, idxs_z = get_density_CIC( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_y, nCells_z, dx, dy, dz, c_pos_x, c_pos_y, c_pos_z )
   phi = get_potential( rho, G, fft_plan_fwd, fft_plan_bkwd)
-  update_particles_NGP( dt, phi, idxs_x, idxs_y, idxs_z, p_inside, p_pos_x, p_pos_y, p_pos_z, p_vel_x, p_vel_y, p_vel_z,  )
-  # if ( mod( nStep, 2  ) == 0 )
-  #   cut = Int(nCells_z/2)
-  #   img = log( 1e6*rho[cut,:,:] + 1 )
-  #   clf()
-  #   imshow(img)
-  #   colorbar()
-  #   title( "Density" )
-  #   savefig("images/density_$(nStep/2 -1 ).png")
-  #   img = phi[cut,:,:]
-  #   clf()
-  #   imshow(img)
-  #   colorbar()
-  #   title( "Potential" )
-  #   savefig("images/potential_$(nStep/2 -1 ).png")
-  # end
+  update_particles_CIC( dt,  dx, dy, dz, phi, idxs_x, idxs_y, idxs_z, p_inside, p_pos_x, p_pos_y, p_pos_z, p_vel_x, p_vel_y, p_vel_z, c_pos_x, c_pos_y, c_pos_z  )
+  if ( mod( nStep, 2  ) == 0 )
+    plot_slides( nStep, Int(nCells_z/2), rho, phi )
+  end
 end
 
 
 
+function advance_step_NGP( nStep, nCells_x, nCells_y, nCells_z, dx, dy, dz, p_inside,
+                           p_pos_x, p_pos_y, p_pos_z, p_vel_x, p_vel_y, p_vel_z,
+                           G, fft_plan_fwd, fft_plan_bkwd )
+  get_particles_outside_NPG( p_inside, p_pos_x, p_pos_y, p_pos_z, x_min, x_max, y_min, y_max, z_min, z_max)
+  rho, idxs_x, idxs_y, idxs_z = get_density_NGP( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_y, nCells_z, dx, dy, dz )
+  phi = get_potential( rho, G, fft_plan_fwd, fft_plan_bkwd)
+  update_particles_NGP( dt, phi, idxs_x, idxs_y, idxs_z, p_inside, p_pos_x, p_pos_y, p_pos_z, p_vel_x, p_vel_y, p_vel_z,  )
+  if ( mod( nStep, 2  ) == 0 )
+    plot_slides( nStep, Int(nCells_z/2), rho, phi )
+  end
+
+end
+
+
+###################################################################################
+G, fft_plan_fwd, fft_plan_bkwd = initialize_FFT( nCells_x, nCells_y, nCells_z, Lx, Ly, Lz, dx, dy, dz )
+
+# First half-step for leapfrog method
+get_particles_outside_CIC( p_inside, p_pos_x, p_pos_y, p_pos_z, x_min, x_max, y_min, y_max, z_min, z_max, dx, dy, dz)
+# rho, idxs_x, idxs_y, idxs_z = get_density_NGP( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_y, nCells_z, dx, dy, dz )
+rho, idxs_x, idxs_y, idxs_z = get_density_CIC( p_inside, p_pos_x, p_pos_y, p_pos_z, nCells_x, nCells_y, nCells_z, dx, dy, dz, c_pos_x, c_pos_y, c_pos_z )
+phi = get_potential( rho, G, fft_plan_fwd, fft_plan_bkwd)
+for i in 1:nParticles
+  if !p_inside[i]
+    continue
+  end
+  idx_x, idx_y, idx_z = idxs_x[i], idxs_y[i], idxs_z[i]
+  g_x, g_y, g_z = get_grav_force_NGP( phi, idx_x, idx_y, idx_z )
+  p_vel_x[i] = p_vel_x[i] + 0.5*dt*g_x
+  p_vel_y[i] = p_vel_y[i] + 0.5*dt*g_y
+  p_vel_z[i] = p_vel_z[i] + 0.5*dt*g_z
+end
+##################################################################################
+# Start simulation
 tic()
 time_total = 0
 stepsPerWrite = 10
 for nStep in 1:totalSteps
   printProgress( nStep-1, totalSteps, time_total)
-  time_step = @elapsed advance_step_NGP( nStep, nCells_x, nCells_y, nCells_z, dx, dy, dz, p_inside, p_pos_x, p_pos_y, p_pos_z, p_vel_x, p_vel_y, p_vel_z,  G, fft_plan_fwd, fft_plan_bkwd )
+  time_step = @elapsed advance_step_CIC( nStep, nCells_x, nCells_y, nCells_z, dx, dy, dz, p_inside, p_pos_x, p_pos_y, p_pos_z, p_vel_x, p_vel_y, p_vel_z, c_pos_x, c_pos_y, c_pos_z, G, fft_plan_fwd, fft_plan_bkwd )
   time_total += time_step
-  if ( mod( nStep, stepsPerWrite  ) == 0 )
-    writeSnapshot( nStep, "", [p_pos_x, p_pos_y, p_pos_z], outFile, stride=8 )
-  end
+  # if ( mod( nStep, stepsPerWrite  ) == 0 )
+  #   writeSnapshot( nStep, "", [p_pos_x, p_pos_y, p_pos_z], outFile, stride=8 )
+  # end
 end
 printProgress( totalSteps, totalSteps, time_total)
 println("\nTotal Time: $(time_total) secs")
 toc()
 
-close( outFile )
+# close( outFile )
